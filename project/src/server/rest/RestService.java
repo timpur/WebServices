2d69135357b8a77d2e5edf4eb257ff285dbca237
timpur <timothy@purchas.com>
2016-05-24 08:39:10 +1000

    Add rest calls.
    Added error capture for Polls Controller get poll by index.

--- project/src/server/rest/RestService.java
+++ project/src/server/rest/RestService.java
@@ -7,17 +7,28 @@ import bean.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
-
 @Path("/service")
 public class RestService {
-	
-	@Path("polls")
+
+	@Path("/polls")
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public List<Poll> getPoll(){
+	public List<Poll> getPollS() {
 		return ApplicationController.PC.getPolls();
 	}
-	
-	
+
+	@Path("/poll/{index}")
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public Poll getPollByIndex(@PathParam("index") int index) {
+		return ApplicationController.PC.getPollByIndex(index);
+	}
+
+	@Path("/poll/id/{ID}")
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public Poll getPollByID(@PathParam("ID") int ID) {
+		return ApplicationController.PC.getPollByID(ID);
+	}
 
 }
